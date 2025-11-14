9c3e7548e508763b0d0af5b3d6e69d0d34b670b1
Tam <tamnguyen2120@gmail.com>
2025-11-14 13:16:25 +0100

    correction views+livre

--- biblio/src/main/java/biblio/view/Views.java
+++ biblio/src/main/java/biblio/view/Views.java
@@ -7,4 +7,22 @@ public class Views {
 	public class Common {} 
 	
 	public class editeurWithLivres extends Editeur{}
+
+	public class Auteur extends Common{}
+
+	public class Editeur extends Common{}
+
+	public class Collection extends Common{}
+
+	public class collectionWithLivres extends Collection{}
+
+	public class auteurWithLivres extends Auteur{}
+
+	public class Livre extends Common{}
+	
+	//public class livreWithAuteur extends Livre{}
+
+	//public class livreWithEditeur extends Livre{}
+
+	//public class livreWithCollection extends Livre{}
 }
