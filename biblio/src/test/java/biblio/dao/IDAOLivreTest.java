2e0dc6695b18b1a8dc1cf2c7cb7fcde200a27863
Jonah2111 <gruson5@gmail.com>
2025-11-14 15:50:21 +0100

    IDAO Livre et IDAO personne

--- biblio/src/test/java/biblio/dao/IDAOLivreTest.java
+++ biblio/src/test/java/biblio/dao/IDAOLivreTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import biblio.model.Auteur;
+import biblio.model.Collection;
+import biblio.model.Editeur;
 import biblio.model.Livre;
 
 @DataJpaTest
@@ -15,20 +18,91 @@ import biblio.model.Livre;
 public class IDAOLivreTest {
 	
 	@Autowired
-    private IDAOLivre dao;
+    private IDAOLivre daoLivre;
+	@Autowired
+    private IDAOAuteur daoAuteur;
+	@Autowired
+    private IDAOCollection daoCollection;
+	@Autowired
+    private IDAOEditeur daoEditeur;
+	
+	private Auteur auteur;
+    private Editeur editeur;
+    private Collection collection;
+    private Livre livre;
 
     @Test
     void testFindByTitre() {
-        // préparer un livre en mémoire
         Livre l1 = new Livre();
         l1.setTitre("Test Livre");
         l1.setResume("Résumé du test livre");
         l1.setAnnee(2023);
-        dao.save(l1);
+        daoLivre.save(l1);
 
-        List<Livre> result = dao.findByTitre("Test Livre");
+        List<Livre> result = daoLivre.findByTitre("Test Livre");
         assertThat(result).isNotEmpty();
         assertThat(result.get(0).getTitre()).isEqualTo("Test Livre");
+    }
+    
+    @Test
+    void testFindByAuteur() {
+    	
+    	auteur = new Auteur("Dupont", "Jean", "FR");
+        editeur = new Editeur("Gallimard", "France");
+        collection = new Collection("Classiques");
+        daoAuteur.save(auteur);
+        daoEditeur.save(editeur);
+        daoCollection.save(collection);
+        
+        livre = new Livre("Test Livre","Résumé du test livre",2023,auteur,editeur,collection); 
+    	
+        daoLivre.save(livre);
+        
+    	List<Livre> result = daoLivre.findByAuteur(auteur);
+    	assertThat(result).isNotEmpty();
+        assertThat(result.get(0).getAuteur().getNom()).isEqualTo("Dupont");
+    	
+    }
+    
+    @Test
+    void testFindByEditeur() {
+    	
+    	auteur = new Auteur("Dupont", "Jean", "FR");
+        editeur = new Editeur("Gallimard", "France");
+        collection = new Collection("Classiques");
+        daoAuteur.save(auteur);
+        daoEditeur.save(editeur);
+        daoCollection.save(collection);
+        
+        livre = new Livre("Test Livre","Résumé du test livre",2023,auteur,editeur,collection); 
+    	
+        daoLivre.save(livre);
+        
+    	List<Livre> result = daoLivre.findByEditeur(editeur);
+    	assertThat(result).isNotEmpty();
+        assertThat(result.get(0).getEditeur().getNom()).isEqualTo("Gallimard");
+    	
+    }
+    
+    @Test
+    void testFindByCollection() {
+    	
+    	auteur = new Auteur("Dupont", "Jean", "FR");
+        editeur = new Editeur("Gallimard", "France");
+        collection = new Collection("Classiques");
+        daoAuteur.save(auteur);
+        daoEditeur.save(editeur);
+        daoCollection.save(collection);
+        
+        livre = new Livre("Test Livre","Résumé du test livre",2023,auteur,editeur,collection); 
+    	
+        daoLivre.save(livre);
+        
+    	List<Livre> result = daoLivre.findByCollection(collection);
+    	assertThat(result).isNotEmpty();
+        assertThat(result.get(0).getCollection().getNom()).isEqualTo("Classiques");
+    	
+    	
     }
 
 }
