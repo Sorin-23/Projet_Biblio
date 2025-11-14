bdcd6a90db1c897c5e71deecb7b8685b6ca5eccb
Sorin.M <sorin.makarich@gmail.com>
2025-11-14 12:54:08 +0100

    collection.java

---
+++ biblio/src/main/java/biblio/model/Collection.java
@@ -0,0 +1,50 @@
+package biblio.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="collection")
+public class Collection {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 200)
+	private String nom;
+	
+	public Collection() {}
+
+	public Collection(Integer id, String nom) {
+		this.id = id;
+		this.nom = nom;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Collection [id=" + id + ", nom=" + nom + "]";
+	}
+	
+
+}
