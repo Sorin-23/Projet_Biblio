d45145f5c09e65a907eff35af45dcc8727ec1663
Jonah2111 <gruson5@gmail.com>
2025-11-17 10:21:41 +0100

    APP.html et navigation réalisé

--- biblio-front/src/app/app.ts
+++ biblio-front/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from './page/navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('biblio-front');
+ protected title : string = "Bienvenue sur notre projet angular";
+
 }
