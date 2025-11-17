098ff245b4ba234f8f388b24c532ca43993f16c8
Jonah2111 <gruson5@gmail.com>
2025-11-17 10:38:18 +0100

    Collection page ts et html

--- biblio-front/src/app/page/collection-page/collection-page.ts
+++ biblio-front/src/app/page/collection-page/collection-page.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CollectionService } from '../../service/collection-service';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CollectionDto } from '../../dto/collection-dto';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-collection-page',
-  imports: [],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule ],
   templateUrl: './collection-page.html',
   styleUrl: './collection-page.css',
 })
-export class CollectionPage {
+export class CollectionPage implements OnInit {
+   protected collections$!: Observable<CollectionDto[]>;
+  protected collectionForm!: FormGroup;
+  protected labelCtrl!: FormControl;
+  protected editingCollection!: CollectionDto | null;
+
+  constructor(private collectionService: CollectionService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.collections$ = this.collectionService.findAll();
+
+    this.labelCtrl = this.formBuilder.control('', Validators.required);
+
+    this.collectionForm = this.formBuilder.group({
+      label: this.labelCtrl
+    });
+  }
+
+  public trackCollection(index: number, value: CollectionDto) {
+    return value.id;
+  }
+
+  public creerOuModifier() {
+    if (this.editingCollection) {
+      this.collectionService.save(new CollectionDto(this.editingCollection.id, this.labelCtrl.value));
+    }
+
+    else {
+      this.collectionService.save(new CollectionDto(0, this.labelCtrl.value));
+      console.log("ajouté ligne bdd");
+    }
+
+    this.editingCollection = null;
+    this.labelCtrl.setValue("");
+  }
+
+  public editer(collection: CollectionDto) {
+    this.editingCollection = collection;
+    this.labelCtrl.setValue(collection.nom);
+  }
+
+  public supprimer(collection: CollectionDto) {
+    this.collectionService.deleteById(collection.id);
+  }
 
 }
